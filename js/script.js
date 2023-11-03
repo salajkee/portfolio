2753d3f6455a23b10a1f08a0f622ebbe4ff45a8e
Salamat <salajkee@mail.ru>
2023-11-03 05:04:01 +0500

    second commit, fix links

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ gsap.registerPlugin(ScrollTrigger)
 const tl = gsap.timeline()
 
 // Smooth Scroll Animation
-let bodyScrollBar = Scrollbar.init(document.body, {
+let bodyScrollBar = Scrollbar.init(document.querySelector('.wrapper'), {
 	damping: 0.07,
 	delegateTo: document,
 })
@@ -17,22 +17,13 @@ ScrollTrigger.scrollerProxy('.wrapper', {
 })
 bodyScrollBar.addListener(ScrollTrigger.update)
 
-// Intro animation
-tl.fromTo(
-	'.header__logo',
-	{
-		y: -50,
-		opacity: 0,
-	},
-	{
-		y: 0,
-		opacity: 1,
-		duration: 0.5,
-	},
-	0.4
-)
-	.fromTo(
-		'.header__nav ul li',
+// Laptop Animation
+const laptopScreen = window.matchMedia('(min-width: 992px)')
+
+if (laptopScreen.matches) {
+	// Intro animation
+	tl.fromTo(
+		'.header__logo',
 		{
 			y: -50,
 			opacity: 0,
@@ -41,177 +32,220 @@ tl.fromTo(
 			y: 0,
 			opacity: 1,
 			duration: 0.5,
-			stagger: 0.15,
 		},
 		0.4
 	)
-	.fromTo(
-		'.intro__head',
-		{
-			y: 50,
-			opacity: 0,
+		.fromTo(
+			'.header__nav ul li',
+			{
+				y: -50,
+				opacity: 0,
+			},
+			{
+				y: 0,
+				opacity: 1,
+				duration: 0.5,
+				stagger: 0.15,
+			},
+			0.4
+		)
+		.fromTo(
+			'.intro__head',
+			{
+				y: 50,
+				opacity: 0,
+			},
+			{
+				y: 0,
+				opacity: 1,
+				duration: 0.5,
+			},
+			0.8
+		)
+		.fromTo(
+			'.intro__title',
+			{
+				y: 50,
+				opacity: 0,
+			},
+			{
+				y: 0,
+				opacity: 1,
+				duration: 0.5,
+			},
+			1
+		)
+		.fromTo(
+			'.intro__btn',
+			{
+				y: 50,
+				opacity: 0,
+			},
+			{
+				y: 0,
+				opacity: 1,
+				duration: 0.5,
+			},
+			1.2
+		)
+
+	// Skills scroll animation
+	gsap.from('.skills__head', {
+		scrollTrigger: {
+			trigger: '.skills',
+			scroller: '.wrapper',
+			start: '-20% 50%',
+			// toggleActions: 'play none none reset',
 		},
-		{
-			y: 0,
-			opacity: 1,
-			duration: 0.5,
+		y: 100,
+		opacity: 0,
+		duration: 0.5,
+	})
+
+	gsap.from('.skills__title', {
+		scrollTrigger: {
+			trigger: '.skills',
+			scroller: '.wrapper',
+			start: '0 50%',
+			// toggleActions: 'play none none reset',
 		},
-		0.8
-	)
-	.fromTo(
-		'.intro__title',
-		{
-			y: 50,
-			opacity: 0,
+		y: 100,
+		opacity: 0,
+		duration: 0.5,
+	})
+
+	const skillsListItem = gsap.utils.toArray('.skills__list li')
+
+	gsap.from(skillsListItem, {
+		scrollTrigger: {
+			trigger: '.skills',
+			scroller: '.wrapper',
+			start: '10% center',
+			// toggleActions: 'play none none reset',
 		},
-		{
-			y: 0,
-			opacity: 1,
-			duration: 0.5,
+		y: 100,
+		opacity: 0,
+		stagger: 0.15,
+		duration: 0.5,
+	})
+
+	const skillsItems = gsap.utils.toArray('.skills__item')
+
+	gsap.from(skillsItems, {
+		scrollTrigger: {
+			trigger: '.skills',
+			scroller: '.wrapper',
+			start: '10% center',
+			// toggleActions: 'play none none reset',
 		},
-		1
-	)
-	.fromTo(
-		'.intro__btn',
-		{
-			y: 50,
-			opacity: 0,
+		opacity: 0,
+		stagger: 0.15,
+		duration: 0.5,
+	})
+
+	// Projects scroll animation
+	gsap.from('.projects__head', {
+		scrollTrigger: {
+			trigger: '.projects',
+			scroller: '.wrapper',
+			start: '-100 center',
+			// toggleActions: 'play none none reset',
 		},
-		{
-			y: 0,
-			opacity: 1,
-			duration: 0.5,
+		y: 100,
+		opacity: 0,
+		duration: 0.5,
+	})
+
+	const projectItems = gsap.utils.toArray('.projects__item')
+
+	gsap.from(projectItems, {
+		scrollTrigger: {
+			trigger: '.projects',
+			scroller: '.wrapper',
+			start: '0 center',
+			// toggleActions: 'play none none reset',
 		},
-		1.2
-	)
+		y: 100,
+		opacity: 0,
+		stagger: 0.3,
+		duration: 1,
+	})
 
-// Skills scroll animation
-gsap.from('.skills__head', {
-	scrollTrigger: {
-		trigger: '.skills',
-		scroller: '.wrapper',
-		start: '-20% 50%',
-		// toggleActions: 'play none none reset',
-	},
-	y: 100,
-	opacity: 0,
-	duration: 0.5,
-})
+	// Footer scroll animation
+	gsap.from('.footer__title', {
+		scrollTrigger: {
+			trigger: '.footer',
+			scroller: '.wrapper',
+			start: '-50 center',
+			// toggleActions: 'play none none reset',
+		},
+		y: 100,
+		opacity: 0,
+		duration: 0.5,
+	})
 
-gsap.from('.skills__title', {
-	scrollTrigger: {
-		trigger: '.skills',
-		scroller: '.wrapper',
-		start: '0 50%',
-		// toggleActions: 'play none none reset',
-	},
-	y: 100,
-	opacity: 0,
-	duration: 0.5,
-})
+	gsap.from('.footer__email', {
+		scrollTrigger: {
+			trigger: '.footer',
+			scroller: '.wrapper',
+			start: '50 center',
+			// toggleActions: 'play none none reset',
+		},
+		y: 100,
+		opacity: 0,
+		duration: 0.5,
+	})
 
-const skillsListItem = gsap.utils.toArray('.skills__list li')
+	const socialLinks = gsap.utils.toArray('.footer__social-link')
 
-gsap.from(skillsListItem, {
-	scrollTrigger: {
-		trigger: '.skills',
-		scroller: '.wrapper',
-		start: '10% center',
-		// toggleActions: 'play none none reset',
-	},
-	y: 100,
-	opacity: 0,
-	stagger: 0.15,
-	duration: 0.5,
-})
+	gsap.from(socialLinks, {
+		scrollTrigger: {
+			trigger: '.footer',
+			scroller: '.wrapper',
+			start: '50 center',
+			// toggleActions: 'play none none reset',
+		},
+		y: 50,
+		opacity: 0,
+		stagger: 0.1,
+		duration: 0.5,
+	})
+}
 
-const skillsItems = gsap.utils.toArray('.skills__item')
+// find all links and animate to the right position
+gsap.utils.toArray('.link').forEach(link => {
+	const target = link.getAttribute('href')
 
-gsap.from(skillsItems, {
-	scrollTrigger: {
-		trigger: '.skills',
-		scroller: '.wrapper',
-		start: '10% center',
-		// toggleActions: 'play none none reset',
-	},
-	opacity: 0,
-	stagger: 0.15,
-	duration: 0.5,
-})
-
-// Projects scroll animation
-gsap.from('.projects__head', {
-	scrollTrigger: {
-		trigger: '.projects',
-		scroller: '.wrapper',
-		start: '-100 center',
-		// toggleActions: 'play none none reset',
-	},
-	y: 100,
-	opacity: 0,
-	duration: 0.5,
-})
-
-const projectItems = gsap.utils.toArray('.projects__item')
-
-gsap.from(projectItems, {
-	scrollTrigger: {
-		trigger: '.projects',
-		scroller: '.wrapper',
-		start: '0 center',
-		// toggleActions: 'play none none reset',
-	},
-	y: 100,
-	opacity: 0,
-	stagger: 0.3,
-	duration: 1,
-})
-
-// Footer scroll animation
-gsap.from('.footer__title', {
-	scrollTrigger: {
-		trigger: '.footer',
-		scroller: '.wrapper',
-		start: '-50 center',
-		// toggleActions: 'play none none reset',
-	},
-	y: 100,
-	opacity: 0,
-	duration: 0.5,
-})
-
-gsap.from('.footer__email', {
-	scrollTrigger: {
-		trigger: '.footer',
-		scroller: '.wrapper',
-		start: '50 center',
-		// toggleActions: 'play none none reset',
-	},
-	y: 100,
-	opacity: 0,
-	duration: 0.5,
-})
-
-const socialLinks = gsap.utils.toArray('.footer__social-link')
-
-gsap.from(socialLinks, {
-	scrollTrigger: {
-		trigger: '.footer',
-		scroller: '.wrapper',
-		start: '50 center',
-		// toggleActions: 'play none none reset',
-	},
-	y: 50,
-	opacity: 0,
-	stagger: 0.1,
-	duration: 0.5,
+	link.addEventListener('click', e => {
+		e.preventDefault()
+		bodyScrollBar.scrollIntoView(document.querySelector(target), {
+			damping: 0.07,
+			offsetTop: 0,
+		})
+	})
 })
 
 // Burger mnenu
 const hamburger = document.querySelector('.hamburger')
 const menu = document.querySelector('.header__nav')
+const wrapper = document.querySelector('.wrapper')
+const navLink = document.querySelectorAll('.header__nav-link')
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('hamburger--active')
 	menu.classList.toggle('header__nav--active')
+
+	if (menu.classList.contains('header__nav--active')) {
+		wrapper.classList.add('no-scroll')
+
+		navLink.forEach(link => {
+			link.addEventListener('click', () => {
+				wrapper.classList.remove('no-scroll')
+				hamburger.classList.remove('hamburger--active')
+				menu.classList.remove('header__nav--active')
+			})
+		})
+	} else {
+		wrapper.classList.remove('no-scroll')
+	}
 })
